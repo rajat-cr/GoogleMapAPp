2d525be70e1a3c0d4878367cdc6e7beefd6a02ee
rajat-cr <rajat@coderroots.com>
2026-03-16 15:12:12 +0530

    Initial commit

---
+++ app/src/main/java/com/rajat/googlemap/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.rajat.googlemap
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Looper
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
+class MainActivity : ComponentActivity() {
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        getLocationPermission()
+        setContent {
+            GoogleMapScreen(fusedLocationProviderClient, this)
+        }
+    }
+    private fun getLocationPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+        else
+            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),101)
+    }
+}
+
+@Composable
+fun GoogleMapScreen(
+    fusedLocationProviderClient: FusedLocationProviderClient,
+    activity: MainActivity
+) {
+    var userLocation by remember { mutableStateOf<LatLng?>(null) }
+    val uiSettings = remember {
+        MapUiSettings(
+            zoomControlsEnabled = true,
+            zoomGesturesEnabled = true
+        )
+    }
+    LaunchedEffect(Unit) {
+        if (ActivityCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) return@LaunchedEffect
+
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            2000
+        ).setMaxUpdates(1).build()
+
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+            location?.let {
+                userLocation = LatLng(it.latitude, it.longitude)
+
+            }
+        }
+    }
+
+    userLocation?.let { location ->
+
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(location, 15f)
+        }
+
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+         //   uiSettings = uiSettings,
+            cameraPositionState = cameraPositionState
+        ) {
+
+            Marker(
+                state = MarkerState(position = location),
+                title = "Your Location"
+            )
+        }
+    }
+}
